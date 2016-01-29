e30c16501716d3e5ef27cdb8cbd3e66943964e90
yupi2 <yupi2@openmailbox.org>
2016-01-29 10:23:00 -0600

    corrected message parsing for unicode

--- lua/autorun/client/cl_cdmessages.lua
+++ lua/autorun/client/cl_cdmessages.lua
@@ -1,11 +1,29 @@
 local function utf8ToTable(str)
-	local tbl = {}
+	local ret = {}
 
 	for p, c in utf8.codes(str) do
-		table.insert(tbl, c)
+		table.insert(ret, c)
 	end
 
-	return tbl
+	return ret
+end
+
+-- sort of table.sub because it doesn't follow string.sub
+--  exactly because I don't need it to
+local function tableSubToUtf8(tbl, startPos, endPos)
+	endPos = endPos or #tbl
+
+	if (startPos > endPos) or (startPos < 1) or (endPos < 1) then
+		return
+	end
+
+	local ret = ""
+
+	for i = startPos, endPos do
+		ret = ret .. utf8.char(tbl[i])
+	end
+
+	return ret
 end
 
 local function rgbStrToColor(str)
@@ -17,91 +35,84 @@ end
 
 -- If last table value is string then append this string to it.
 local function tableInsertString(tbl, str)
-	if #tbl == 0 then
+	if str and str ~= "" then
 		table.insert(tbl, str)
-	else
-		local v = tbl[#tbl]
-		if isstring(v) then
-			tbl[#tbl] = v .. str
-		else
-			table.insert(tbl, str)
-		end
 	end
 end
 
 -- If last table value is color-table then overwrite that shit.
 local function tableInsertColor(tbl, clr)
-	if #tbl == 0 then
-		table.insert(tbl, clr)
-	else
-		local v = tbl[#tbl]
-		if isstring(v) then
-			tbl[#tbl] = clr
-		else
-			table.insert(tbl, clr)
-		end
+	local v = tbl[#tbl]
+
+	if istable(v) then
+		tbl[#tbl] = clr
+		return
 	end
+
+	table.insert(tbl, clr)
 end
 
 -- Parse messages with colors into a table for chat.AddText().
+-- This obviously throws errors if you give an incorrect color in msg or
+--  you don't use "##" in msg to represent a single "#".
 -- EXAMPLES:
 --   "foo ## bar" -> {"foo # bar"}
 --   "and #ff0000cool" -> {"and ", Color(255, 0, 0), "cool"}
---   "no colors ##AABBCC" -> "no colors #AABBCC"
+--   "no colors ##AABBCC" -> {"no colors #AABBCC"}
 local function processColorMessage(msg)
 	local chars = utf8ToTable(msg)
 	local ret = {}
 	local y = 1
 
-	local i = 0
-	while i <= #chars do
-		i = i + 1
-
-		-- char
+	local i = 1
+	while #chars >= i do
 		local c = utf8.char(chars[i])
+
+		i = i + 1
 
 		if c ~= "#" then
 			continue
 		end
 
-		i = i + 1
-
-		-- next char
 		local nc = utf8.char(chars[i])
 		assert(nc ~= "")
 
 		-- handle '##'
 		if nc == "#" then
-			tableInsertString(ret, msg:sub(y, i))
-			y = i + 1
+			tableInsertString(ret, tableSubToUtf8(chars, y, i-1))
+			i = i + 1
+			y = i
 			continue
 		end
 
-		local h = msg:sub(i, i + 5)
+		local h = tableSubToUtf8(chars, i, i + 5)
 
 		-- Matches six hexadecimal digits.
 		-- EXAMPLE: "abc123"
 		assert(h:match("%x%x%x%x%x%x", 1) ~= nil)
 
-		if not istable(ret[#ret]) then
-			tableInsertString(ret, msg:sub(y, i - 2))
+		-- insert the string before the color into ret
+		tableInsertString(ret, tableSubToUtf8(chars, y, i - 2))
 
-			table.insert(ret, rgbStrToColor(h))
-		end
+		-- insert the color table into ret
+		tableInsertColor(ret, rgbStrToColor(h))
 
 		i = i + 5
 		y = i + 1
 	end
 
-	local v = msg:sub(y)
-	if v ~= "" then
+	-- add in the rest of the string to ret
+	local v = tableSubToUtf8(chars, y)
+	if v and v ~= "" then
 		tableInsertString(ret, v)
 	end
+
+	PrintTable(ret)
 
 	return ret
 end
 
-local function ProcessCDMessage()
+net.Receive("connect_disconnect_message", function()
 	local msgLength = net.ReadUInt(16)
 
 	if msgLength == 0 then
@@ -110,9 +121,5 @@ local function ProcessCDMessage()
 
 	local msg = util.Decompress(net.ReadData(msgLength))
 
-	chat.Add(unpack(processColorMessage(msg)))
-end
-
-net.Receive("cdmessage_player_connected", ProcessCDMessage)
-net.Receive("cdmessage_player_connecting", ProcessCDMessage)
-net.Receive("cdmessage_player_disconnected", ProcessCDMessage)
+	chat.AddText(unpack(processColorMessage(msg)))
+end)
